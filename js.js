9376bdd59bd4eba444fbb444f68be7595833b622
Roy Daniel <roydaniel@Roys-MacBook-Pro.local>
2021-11-29 14:41:16 -0500

    2021_11_29 mvp

---
+++ js.js
@@ -0,0 +1,67 @@
+const player = {
+  playerChoice:null
+}
+
+const computer = {
+  computerChoice:null
+}
+
+let newGame = (playerSelection) => {
+  
+  player.playerChoice = choices[playerSelection];
+  playerDisplay.append(player.playerChoice + " ");
+  
+  computer.computerChoice = choices[computerChooses()];
+  compDisplay.append(computer.computerChoice + " ");
+  
+  resultDisplay.append(compareChoices() + " ");
+  compareChoices();
+}
+
+let computerChooses = () => {
+  let randomIndex = Math.floor(Math.random() * 3);
+  return randomIndex;
+}
+
+const compareChoices = () => {
+  let result = null;
+  if (player.playerChoice === computer.computerChoice) {
+    result ="It's a tie!";
+  }
+  else if (computer.computerChoice === choices[0]) {
+    if (player.playerChoice === choices[1]) {
+      result ="Player won - paper beats rock";
+    }
+    else if (player.playerChoice === choices[2]) {
+      result ="Player lost - rock beats scissors";
+    }
+  }
+  else if (computer.computerChoice === choices[1]) {
+    if (player.playerChoice === choices[0]) {
+      result ="Player lost - paper beats rock";
+    }
+    else if (player.playerChoice === choices[2]) {
+      result ="Player won - scissors beats paper";
+    }
+  }
+  else if (computer.computerChoice === choices[2]) {
+    if (player.playerChoice === choices[0]) {
+      result ="Player won - Rock beats Scissors";
+    }
+    else if (player.playerChoice === choices[1]) {
+      result = "Player lost - scissors beats paper";
+    }
+  }
+  return result;
+}
+
+const choices = ["Lapis","Papyrus","Scalpellus"];
+
+let compDisplay = document.querySelector(".computerSelect");
+let playerDisplay = document.querySelector(".playerSelect");
+let resultDisplay = document.querySelector(".result");
+
+document.getElementById("lapis").addEventListener('click', function(){newGame(0)});
+document.getElementById("papyrus").addEventListener('click', function(){newGame(1)});
+
+document.getElementById("scalpellus").addEventListener('click',function(){newGame(2)});
